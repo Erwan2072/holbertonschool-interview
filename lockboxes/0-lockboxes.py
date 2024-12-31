d796a94b6b6b153158a3fabf56e65ea33c2f8856
Erwan2072 <erwan.lebreton@hotmail.fr>
2024-12-31 11:36:15 +0100

    correction pycodestyle

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -23,14 +23,3 @@ def canUnlockAll(boxes):
                 keys.append(key)
 
     return all(unlocked)
-
-# Testing the implementation
-if __name__ == "__main__":
-    boxes = [[1], [2], [3], [4], []]
-    print(canUnlockAll(boxes))  # True
-
-    boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
-    print(canUnlockAll(boxes))  # True
-
-    boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
-    print(canUnlockAll(boxes))  # False
