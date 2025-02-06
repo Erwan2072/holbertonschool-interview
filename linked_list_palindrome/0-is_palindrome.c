e55663272713bc16ab73a8d5ede05e23b924bc1e
Erwan2072 <erwan.lebreton@hotmail.fr>
2025-02-06 08:58:28 +0100

    correction indentation

--- linked_list_palindrome/0-is_palindrome.c
+++ linked_list_palindrome/0-is_palindrome.c
@@ -1,61 +1,63 @@
-#include "lists.h"
+#include <stdio.h>
 #include <stdlib.h>
-
 #include "lists.h"
-#include <stdlib.h>
 
 /**
- * reverse_list - Reverse a linked list.
- * @head: Pointer to the head of the list.
+ * reverse_list - Reverses a singly linked list
+ * @head: Pointer to the head of the list
  *
- * Return: Pointer to the new head of the reversed list.
+ * Return: A pointer to the new head of the reversed list
  */
+
 listint_t *reverse_list(listint_t *head)
 {
-    listint_t *prev = NULL, *next = NULL;
+	listint_t *prev = NULL, *next = NULL;
 
-    while (head)
-    {
-        next = head->next;
-        head->next = prev;
-        prev = head;
-        head = next;
-    }
-    return prev;
+	while (head != NULL)
+	{
+		next = head->next;
+		head->next = prev;
+		prev = head;
+		head = next;
+	}
+	return (prev);
 }
 
 /**
- * is_palindrome - Checks if a singly linked list is a palindrome.
- * @head: Double pointer to the head of the linked list.
+ * is_palindrome - Checks if a singly linked list is a palindrome
+ * @head: Double pointer to the head of the list.
  *
- * Return: 1 if the linked list is a palindrome, 0 otherwise.
+ * Return: 1 if the list is a palindrome, 0 otherwise
  */
+
 int is_palindrome(listint_t **head)
 {
-    listint_t *slow = *head, *fast = *head, *first_half, *second_half;
+	listint_t *slow = *head;
+	listint_t *fast = *head;
+	listint_t *first_half = *head;
+	listint_t *second_half;
 
-    if (*head == NULL || (*head)->next == NULL)
-        return (1);
+	if (head == NULL || *head == NULL)
+		return (1);
 
-    /* 1. Trouver le milieu de la liste */
-    while (fast && fast->next)
-    {
-        slow = slow->next;
-        fast = fast->next->next;
-    }
+	/** Find the middle of the list */
+	while (fast != NULL && fast->next != NULL)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+	}
 
-    /* 2. Inverser la seconde moitié */
-    second_half = reverse_list(slow);
-    first_half = *head;
+	/** Reverse the second half of the list */
+	second_half = reverse_list(slow);
 
-    /* 3. Comparer les deux moitiés */
-    while (second_half)
-    {
-        if (first_half->n != second_half->n)
-            return (0);
-        first_half = first_half->next;
-        second_half = second_half->next;
-    }
-
-    return (1);
+	/** Compare the first half and the reversed second half */
+	while (second_half != NULL)
+	{
+		if (first_half->n != second_half->n)
+			return (0);
+		first_half = first_half->next;
+		second_half = second_half->next;
+	}
+    
+	return (1);
 }
