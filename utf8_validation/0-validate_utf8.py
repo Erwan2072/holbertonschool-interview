dad0a1b64b2f2cdad2c5550f06160145f933176a
Erwan2072 <erwan.lebreton@hotmail.fr>
2025-03-03 15:11:12 +0100

    correction PEP8

--- utf8_validation/0-validate_utf8.py
+++ utf8_validation/0-validate_utf8.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """
-Module that provides a method to determine if a given data set represents a valid UTF-8 encoding.
+Module that provides a method to determine
+if a given data set represents a valid UTF-8 encoding.
 """
 from typing import List
+
 
 def validUTF8(data: List[int]) -> bool:
     num_bytes = 0
