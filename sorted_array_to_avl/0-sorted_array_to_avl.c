ee3199d5b2ddc1bd32f958afdb2151c2e449b532
Erwan2072 <erwan.lebreton@hotmail.fr>
2025-04-03 13:13:01 +0200

    correction for compilation

--- sorted_array_to_avl/0-sorted_array_to_avl.c
+++ sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -1,38 +1,40 @@
-#include "binary_trees.h"
 #include <stdlib.h>
+#include "binary_trees.h"
 
 /**
- * create_avl - Recursively builds AVL tree from sorted array
- * @array: Array of ints
- * @start: Start index
- * @end: End index
- * @parent: Parent node
- * Return: Root node
- */
-avl_t *create_avl(int *array, int start, int end, avl_t *parent)
-{
-    if (start > end)
-        return (NULL);
-
-    int mid = (start + end) / 2;
-    avl_t *root = binary_tree_node(parent, array[mid]);
-    if (!root)
-        return (NULL);
-
-    root->left = create_avl(array, start, mid - 1, root);
-    root->right = create_avl(array, mid + 1, end, root);
-    return (root);
-}
-
-/**
- * sorted_array_to_avl - Creates AVL tree from sorted array
- * @array: Pointer to array
- * @size: Number of elements
- * Return: Root node of AVL tree
+ * sorted_array_to_avl - Builds an AVL tree from a sorted array
+ * @array: Pointer to the first element of the array to be converted
+ * @size: Number of elements in the array
+ *
+ * Return: Pointer to the root node of the created AVL tree, or NULL on failure
  */
 avl_t *sorted_array_to_avl(int *array, size_t size)
 {
-    if (!array || size == 0)
-        return (NULL);
-    return (create_avl(array, 0, (int)size - 1, NULL));
+	avl_t *root;
+	int middle;
+
+	if (size == 0)
+		return (NULL);
+
+	/* Allocate memory for new node */
+	root = malloc(sizeof(avl_t));
+
+	if (root == NULL)
+		return (NULL);
+
+	/* Find middle element of the array */
+	middle = size / 2;
+
+	/* Create root node */
+	root->n = array[middle];
+	root->left = sorted_array_to_avl(array, middle);
+	root->right = sorted_array_to_avl(array + middle + 1, size - middle - 1);
+	root->parent = NULL;
+
+	if (root->left != NULL)
+		root->left->parent = root;
+	if (root->right != NULL)
+		root->right->parent = root;
+
+	return (root);
 }
