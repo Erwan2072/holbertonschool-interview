b77aa4330d698776317504c7a8d4b0501e2245d0
Erwan2072 <erwan.lebreton@hotmail.fr>
2025-04-07 09:07:22 +0200

    add functions file search.h

--- linear_skip/search.h
+++ linear_skip/search.h
@@ -1,15 +1,20 @@
-#ifndef SEARCH_H
-#define SEARCH_H
+#ifndef _SEARCH_
+#define _SEARCH_
 
+#include <stdio.h>
+#include <stdlib.h>
 #include <stddef.h>
 
 /**
  * struct skiplist_s - Singly linked list with an express lane
  *
- * @n: Integer stored at the node
+ * @n: Integer
  * @index: Index of the node in the list
  * @next: Pointer to the next node
  * @express: Pointer to the next node in the express lane
+ *
+ * Description: singly linked list node structure with an express lane
+ * for Holberton project
  */
 typedef struct skiplist_s
 {
@@ -20,5 +25,8 @@ typedef struct skiplist_s
 } skiplist_t;
 
 skiplist_t *linear_skip(skiplist_t *list, int value);
+skiplist_t *create_skiplist(int *array, size_t size);
+void print_skiplist(const skiplist_t *list);
+void free_skiplist(skiplist_t *list);
 
-#endif
+#endif /* _SEARCH_ */
